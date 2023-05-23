76f703b58a5803084401e7ac8965ce8279d03492
rochadiegox <127871082+rochadiegox@users.noreply.github.com>
2023-05-23 14:45:08 -0300

    Update StringsDatas.java

--- StringsDatas.java
+++ StringsDatas.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stringsdatas;
 
 import java.time.LocalDate;
@@ -11,18 +6,10 @@ import java.time.format.TextStyle;
 import static java.time.temporal.TemporalQueries.localDate;
 import java.util.Locale;
 
-/**
- *
- * @author Diego
- */
 public class StringsDatas {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Olá, {nome}. Hoje é {dia-da-semana}, BOM DIA.
+               //Olá, {nome}. Hoje é {dia-da-semana}, BOM DIA.
         
         String nome = "Diego";
         
